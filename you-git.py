bb0240a1df57111c854af4e7cdcaaff75721a3bb
Prompt Engineer <prompt.engineer48@gmail.com>
2023-05-21 15:01:34 +0530

    Update you-git.py

--- you-git.py
+++ you-git.py
@@ -11,23 +11,24 @@ st.title('🦜️🔗 Youtube Summarizer :red[Toolbox]')
 
 import os
 api_key = st.text_input("Paste the OpenAI API Key")
+
 if api_key:
     os.environ["OPENAI_API_KEY"] = api_key
 
-llm = OpenAI(temperature=0) #Temp controls the randomness of the text
+    llm = OpenAI(temperature=0) #Temp controls the randomness of the text
 
-prompt = st.text_input("Paste the URL")
+    prompt = st.text_input("Paste the URL")
 
-if prompt:
-    loader = YoutubeLoader.from_youtube_url(prompt, add_video_info=False)
+    if prompt:
+        loader = YoutubeLoader.from_youtube_url(prompt, add_video_info=False)
 
-    docs = loader.load()
+        docs = loader.load()
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, separator = " ", chunk_overlap=50)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, separator = " ", chunk_overlap=50)
 
-    split_docs = text_splitter.split_documents(docs)
+        split_docs = text_splitter.split_documents(docs)
 
-    chain = load_summarize_chain(llm, chain_type="map_reduce")
-    
-    st.write(chain.run(split_docs))
+        chain = load_summarize_chain(llm, chain_type="map_reduce")
+        
+        st.write(chain.run(split_docs))
 
